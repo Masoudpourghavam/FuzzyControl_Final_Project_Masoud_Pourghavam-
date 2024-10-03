a0e5c19a68ab846aacfe489cbb5b8e4f45d8949a
Masoud <masoodghavam@gmail.com>
2024-10-03 15:05:37 +0330

    first commit

---
+++ MATLAB Regenerated Paper Codes/model/plant.m
@@ -0,0 +1,21 @@
+
+% 
+% -------------------------------------------------
+% Sawyer 4 DoF Manipulator
+% -------------------------------------------------
+% Equation)
+%       ddq = M(q)ddq + c(q, dq)dq + g(q)
+%
+% Input)
+%       x   : Joint Postion and Velocity
+%       u   : Joint Torque
+% Output)
+%       dxdt  : Joint Velocity and Acceleration
+%
+% the following code has been tested on Matlab 2021a
+function dxdt = plant(q, u1,M,C,G)  
+
+ddot = inv(M)*(-C*[q(5);q(6);q(7);q(8)]-G'+u1);
+dxdt = [q(5);q(6);q(7);q(8);ddot(1);ddot(2);ddot(3);ddot(4)];
+
+end
---
+++ MATLAB Regenerated Paper Codes/model/rk2.m
@@ -0,0 +1,22 @@
+
+% 
+% -------------------------------------------------
+% Runge-Kutta Method
+% -------------------------------------------------
+% Equation)
+%       dx = f(x)+Bu
+% Input)
+%       x   : state x
+%       u   : Input u
+%       T   : Simulation Time 
+% Output)
+%       dx  : Differentiation of State x
+%
+% the following code has been tested on Matlab 2021a
+function dx = rk2(x, u1, T,M,C,G)
+k1 = plant(x, u1,M,C,G)*T;
+k2 = plant(x+k1*0.5,u1,M,C,G)*T;
+k3 = plant(x+k2*0.5,u1,M,C,G)*T;
+k4 = plant(x+k3,u1,M,C,G)*T;
+dx = x+((k1+k4)/6 + (k2+k3)/3);
+
